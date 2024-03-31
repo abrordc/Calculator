68827a31fd11d7a5abcfc09a8d8dad4deccc7fc7
abrordc <moh.abrorilhuda@gmail.com>
2024-04-01 05:35:19 +0700

    update code

--- js/index.js
+++ js/index.js
@@ -1,27 +1,36 @@
 
-let input = document.getElementById('inputbox');
-let buttons = document.querySelectorAll('button');
+let input = document.getElementById("inputbox");
+let buttons = document.querySelectorAll("button");
+new Swal({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+    icon: "warning",
+    title: "tombol DEL masih ada bug mohon menggunakan tombol AC",
+    didOpen: toast => {
+        toast.addEventListener("mouseenter", Swal.stopTimer);
+        toast.addEventListener("mouseleave", Swal.resumeTimer);
+    }
+});
 
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-  button.addEventListener('click', (e) => {
-    if(e.target.innerHTML == '=') {
-      string = eval(string);
-      input.value = string;
-    }
-    else if(e.target.innerHTML == 'AC'){
-      String= "";
-      input.value = string;
-      string = string.substring(innerHTML = string);
-    }
-    else if(e.target.innerHTML == 'DEL'){
-      string = string.substring(0, string.length -1);
-    // alert("mohon maaf tombol DEL tidak berfungsi karena dalam masa pengembangan")
-    }
-    else{
-    string += e.target.innerHTML;
-    input.value = string;
-    }
-  });
+    button.addEventListener("click", e => {
+        if (e.target.innerHTML == "=") {
+            string = eval(string);
+            input.value = string;
+        } else if (e.target.innerHTML == "AC") {
+            string = "";
+            input.value = string;
+            string = string.substring((innerHTML = string));
+        } else if (e.target.innerHTML == "DEL") {
+            input.value = input.value.slice(0, -1);
+        } else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    });
 });
